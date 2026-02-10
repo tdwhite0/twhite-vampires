5f43acc286020b850d3498bbf2911ec431e83031
Tom White <tdwhite0@gmail.com>
2026-02-10 10:33:37 -0500

    files

---
+++ twhite-topdown_bg.wasm.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __wasm_bindgen_func_elem_108887: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_105176: (a: number, b: number) => void;
+export const __wasm_bindgen_func_elem_100817: (a: number, b: number, c: number, d: number) => void;
+export const __wasm_bindgen_func_elem_106037: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_108888: (a: number, b: number, c: number) => void;
+export const __wasm_bindgen_func_elem_105177: (a: number, b: number) => void;
+export const __wbindgen_export: (a: number, b: number) => number;
+export const __wbindgen_export2: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export3: (a: number) => void;
+export const __wbindgen_export4: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
